f89d7e0bd8a874a9fd0dd3635abed7a773248fab
Jun Young Cho <godguman@gmail.com>
2020-10-05 10:12:33 +0700

    second Jun

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,42 +2,45 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_API + "/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+	baseURL: process.env.REACT_APP_BACKEND_API + "/api",
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 /**
  * console.log all requests and responses
  */
 api.interceptors.request.use(
-  (request) => {
-    console.log("Starting Request", request);
-    return request;
-  },
-  function (error) {
-    console.log("REQUEST ERROR", error);
-  }
+	(request) => {
+		console.log("Starting Request", request);
+		return request;
+	},
+	function (error) {
+		console.log("REQUEST ERROR", error);
+	}
 );
 
 api.interceptors.response.use(
-  (response) => {
-    console.log("Response:", response);
-    if (response.data.data && response.data.data.accessToken) {
-      api.defaults.headers.common["authorization"] =
-        "Bearer " + response.data.data.accessToken;
-    }
-    return response;
-  },
-  function (error) {
-    error = error.response.data;
-    console.log("RESPONSE ERROR", error);
-    let errorMsg = error.message || "";
-    if (error.errors && error.errors.message)
-      errorMsg = errorMsg + ": " + error.errors.message;
-    toast.error(errorMsg);
-    return Promise.reject(error);
-  }
+	(response) => {
+		console.log("Response:", response);
+		if (response.data.data && response.data.data.accessToken) {
+			api.defaults.headers.common["authorization"] =
+				"Bearer " + response.data.data.accessToken;
+		}
+		return response;
+	},
+	function (error) {
+		error = error.response.data;
+		console.log("RESPONSE ERROR", error);
+		let errorMsg = error.message || "";
+		if (error.errors && error.errors.message)
+			errorMsg = errorMsg + ": " + error.errors.message;
+		toast.error(errorMsg);
+		if (error) {
+			alert(`${errorMsg}`);
+		}
+		return Promise.reject(error);
+	}
 );
 
 export default api;
