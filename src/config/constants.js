a7ac1f395a5ed186aed7eefe5e79d1b19488e107
William <wwantoine@gmail.com>
2020-10-20 16:28:40 +0700

    update4

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const FB_APP_ID = "395659615168657";
 
 export const GOOGLE_CLIENT_ID =
-  "476535208580-9i706cs6bliiuej9ehe4qjom4p1hpi8b.apps.googleusercontent.com";
+  "476535208580-6kkcubh09vtshqbdmmij91pblc39gjo2.apps.googleusercontent.com";
